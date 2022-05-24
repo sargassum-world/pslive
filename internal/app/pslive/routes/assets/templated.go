9f0a9d564c528c63cbf4121ed80d61ae7f78439f
Ethan Li <lietk12@gmail.com>
2022-05-24 00:14:41 -0700

    Make app installable as PWA, cache static assets in service worker

--- internal/app/pslive/routes/assets/templated.go
+++ internal/app/pslive/routes/assets/templated.go
@@ -4,6 +4,8 @@ package assets
 import (
 	"github.com/labstack/echo/v4"
 	"github.com/sargassum-world/fluitans/pkg/godest"
+
+	"github.com/sargassum-world/pslive/internal/app/pslive/auth"
 )
 
 func (h *TemplatedHandlers) getWebmanifest() echo.HandlerFunc {
@@ -15,6 +17,19 @@ func (h *TemplatedHandlers) getWebmanifest() echo.HandlerFunc {
 		return h.r.CacheablePage(
 			c.Response(), c.Request(), t, struct{}{}, struct{}{},
 			godest.WithContentType("application/manifest+json; charset=UTF-8"),
+			godest.WithRevalidateWhenStale(cacheMaxAge),
+		)
+	}
+}
+
+func (h *TemplatedHandlers) getOffline() echo.HandlerFunc {
+	t := "app/offline.page.tmpl"
+	h.r.MustHave(t)
+	return func(c echo.Context) error {
+		const cacheMaxAge = 86400 // 1 day
+		// Produce output
+		return h.r.CacheablePage(
+			c.Response(), c.Request(), t, struct{}{}, auth.Auth{},
 			godest.WithRevalidateWhenStale(cacheMaxAge),
 		)
 	}
---
+++ web/app/src/sw.js
@@ -0,0 +1,62 @@
+import {
+  offlineFallback,
+  staticResourceCache,
+  warmStrategyCache,
+} from 'workbox-recipes';
+import { registerRoute, setDefaultHandler } from 'workbox-routing';
+import { CacheFirst, NetworkOnly } from 'workbox-strategies';
+import { ExpirationPlugin } from 'workbox-expiration';
+
+const cacheFirst = new CacheFirst();
+
+// Static assets (served with file-rev, so cache forever)
+
+staticResourceCache();
+registerRoute(/static\/.*/, cacheFirst);
+registerRoute(/app\/.*/, cacheFirst);
+
+// Favicon (served with max-age cache-control header)
+
+const faviconCacheFirst = new CacheFirst({
+  cacheName: 'favicon-cache',
+  plugins: [
+    new ExpirationPlugin({
+      maxAgeSeconds: 7 * 24 * 60 * 60,
+    }),
+  ],
+})
+warmStrategyCache({
+  urls: ['/favicon.ico'],
+  strategy: faviconCacheFirst,
+});
+registerRoute(/favicon\.ico/, faviconCacheFirst);
+
+// Fonts (served with max-age cache-control header)
+
+const fontCacheFirst = new CacheFirst({
+  cacheName: 'font-cache',
+  plugins: [
+    new ExpirationPlugin({
+      maxAgeSeconds: 365 * 24 * 60 * 60,
+    }),
+  ],
+})
+warmStrategyCache({
+  urls: [
+    '/fonts/atkinson-hyperlegible-latin-400-normal.woff2',
+    '/fonts/atkinson-hyperlegible-latin-700-normal.woff2',
+  ],
+  strategy: fontCacheFirst,
+})
+registerRoute(/fonts\/.*/, fontCacheFirst);
+
+// Pages (no cache)
+
+setDefaultHandler(new NetworkOnly());
+warmStrategyCache({
+  urls: ['/app/offline'],
+  strategy: cacheFirst,
+});
+offlineFallback({
+  pageFallback: '/app/offline',
+});
